a2023a590ccbea13ef19a2d5de6f122d8e0295ef
Steve Paik <spaik@google.com>
2014-06-21 23:18:03 -0700

    Add relative date

--- src/com/codepath/apps/basictwitter/TweetArrayAdapter.java
+++ src/com/codepath/apps/basictwitter/TweetArrayAdapter.java
@@ -1,8 +1,12 @@
 package com.codepath.apps.basictwitter;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import android.content.Context;
+import java.text.ParseException;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +39,7 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
 		ImageView ivProfileImage = (ImageView) v.findViewById(R.id.ivProfileImage);
 		TextView tvUserName = (TextView) v.findViewById(R.id.tvUserName);
 		TextView tvBody = (TextView) v.findViewById(R.id.tvBody);
+		TextView tvRelativeTimestamp = (TextView) v.findViewById(R.id.tvRelativeTimestamp);
 		// Clear out image if it's recycled
 		ivProfileImage.setImageResource(android.R.color.transparent);;
 		ImageLoader imageLoader = ImageLoader.getInstance();
@@ -42,7 +47,25 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
 		imageLoader.displayImage(tweet.getUser().getProfileImageUrl(), ivProfileImage);
 		tvUserName.setText(tweet.getUser().getScreenName());
 		tvBody.setText(tweet.getBody());
+		tvRelativeTimestamp.setText(getRelativeTimeAgo(tweet.getCreatedAt()));
 		return v;
 	}
 	
+	// getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+	public String getRelativeTimeAgo(String rawJsonDate) {
+		String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+		SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+		sf.setLenient(true);
+	 
+		String relativeDate = "";
+		try {
+			long dateMillis = sf.parse(rawJsonDate).getTime();
+			relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+					System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	 
+		return relativeDate;
+	}	
 }
