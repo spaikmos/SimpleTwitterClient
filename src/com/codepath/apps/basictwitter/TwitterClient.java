56c0765dbda898b3c9b49402b20da7af70d85093
Steve Paik <spaik@google.com>
2014-07-02 00:08:19 -0700

    Check network status before sending JSON requests

--- src/com/codepath/apps/basictwitter/TwitterClient.java
+++ src/com/codepath/apps/basictwitter/TwitterClient.java
@@ -4,6 +4,8 @@ import org.scribe.builder.api.Api;
 import org.scribe.builder.api.TwitterApi;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.codepath.oauth.OAuthBaseClient;
@@ -23,72 +25,107 @@ import com.loopj.android.http.RequestParams;
  * 
  */
 public class TwitterClient extends OAuthBaseClient {
-    public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
-    public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-    public static final String REST_CONSUMER_KEY = "oTWoGSJMhxQFuF72hNupyXPdp";       // Change this
-    public static final String REST_CONSUMER_SECRET = "tLEavPABMItOyzwd6UiKYbkKwqUj1EGeXxyPHZt548XmWdARul"; // Change this
-    public static final String REST_CALLBACK_URL = "oauth://cpbasictweets"; // Change this (here and in manifest)
-    
-    public TwitterClient(Context context) {
-        super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
-    }
-    
-    public void GetHomeTimeline(long maxId, long sinceId, JsonHttpResponseHandler handler) {
-    	GetGenericTimeline("home", maxId, sinceId, null, handler);
-    }
-    
-    public void GetAccountCredentials(JsonHttpResponseHandler handler) {
-    	String apiUrl = getApiUrl("account/verify_credentials.json");
-		Log.d("debug", "url - " + apiUrl.toString());
-    	client.get(apiUrl, null, handler);
-    }
-    
-    public void PostUpdate(String tweetText, JsonHttpResponseHandler handler) {
-    	String apiUrl = getApiUrl("statuses/update.json");
-    	RequestParams params = new RequestParams();
-    	params.put("status", tweetText);
-    	client.post(apiUrl, params, handler);
-    }
+	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change
+																				// this
+	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change
+																			// this,
+																			// base
+																			// API
+																			// URL
+	public static final String REST_CONSUMER_KEY = "oTWoGSJMhxQFuF72hNupyXPdp"; // Change
+																				// this
+	public static final String REST_CONSUMER_SECRET = "tLEavPABMItOyzwd6UiKYbkKwqUj1EGeXxyPHZt548XmWdARul"; // Change
+																											// this
+	public static final String REST_CALLBACK_URL = "oauth://cpbasictweets"; // Change
+																			// this
+																			// (here
+																			// and
+																			// in
+																			// manifest)
+	private static Context myContext;
 
-	public void GetMentionsTimeline(long maxId, long sinceId, JsonHttpResponseHandler handler) {
+	public TwitterClient(Context context) {
+		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY,
+				REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+		myContext = context;
+	}
+
+	private Boolean isNetworkAvailable() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) myContext
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager
+				.getActiveNetworkInfo();
+		return activeNetworkInfo != null
+				&& activeNetworkInfo.isConnectedOrConnecting();
+	}
+
+	public void GetHomeTimeline(long maxId, long sinceId,
+			JsonHttpResponseHandler handler) {
+		GetGenericTimeline("home", maxId, sinceId, null, handler);
+	}
+
+	public void GetAccountCredentials(JsonHttpResponseHandler handler) {
+		if (isNetworkAvailable()) {
+			String apiUrl = getApiUrl("account/verify_credentials.json");
+			Log.d("debug", "url - " + apiUrl.toString());
+			client.get(apiUrl, null, handler);
+		}
+	}
+
+	public void PostUpdate(String tweetText, JsonHttpResponseHandler handler) {
+		if (isNetworkAvailable()) {
+			String apiUrl = getApiUrl("statuses/update.json");
+			RequestParams params = new RequestParams();
+			params.put("status", tweetText);
+			client.post(apiUrl, params, handler);
+		}
+	}
+
+	public void GetMentionsTimeline(long maxId, long sinceId,
+			JsonHttpResponseHandler handler) {
 		GetGenericTimeline("mentions", maxId, sinceId, null, handler);
 	}
-    
+
 	public void GetUsersShow(String screenname, JsonHttpResponseHandler handler) {
-		String apiUrl = getApiUrl("users/show.json");
-		RequestParams params = new RequestParams();
-		params.put("screen_name", screenname);
-		client.get(apiUrl, params, handler);
+		if (isNetworkAvailable()) {
+			String apiUrl = getApiUrl("users/show.json");
+			RequestParams params = new RequestParams();
+			params.put("screen_name", screenname);
+			client.get(apiUrl, params, handler);
+		}
 	}
-	
-	public void GetUserTimeline(long maxId, long sinceId, String screenname, JsonHttpResponseHandler handler) {
+
+	public void GetUserTimeline(long maxId, long sinceId, String screenname,
+			JsonHttpResponseHandler handler) {
 		GetGenericTimeline("user", maxId, sinceId, screenname, handler);
 	}
 
-	private void GetGenericTimeline(String timelineName, long maxId, long sinceId, String screenname, JsonHttpResponseHandler handler) {
-    	String apiUrl = getApiUrl("statuses/" + timelineName + "_timeline.json");
-    	RequestParams params = new RequestParams();
-    	if(sinceId != 0) {
-        	params.put("since_id",  String.valueOf(sinceId));
-    	} 
-    	
-    	if(maxId != 0) {
-    		params.put("max_id", String.valueOf(maxId));
-    	}
-    	
-    	if(screenname != null) {
-    		params.put("screen_name", screenname);
-    	}
-    	client.get(apiUrl, params, handler);
-	}
+	private void GetGenericTimeline(String timelineName, long maxId,
+			long sinceId, String screenname, JsonHttpResponseHandler handler) {
+		if (isNetworkAvailable()) {
+			String apiUrl = getApiUrl("statuses/" + timelineName
+					+ "_timeline.json");
+			RequestParams params = new RequestParams();
+			if (sinceId != 0) {
+				params.put("since_id", String.valueOf(sinceId));
+			}
 
-    
-    /* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
-     * 	  i.e getApiUrl("statuses/home_timeline.json");
-     * 2. Define the parameters to pass to the request (query or body)
-     *    i.e RequestParams params = new RequestParams("foo", "bar");
-     * 3. Define the request method and make a call to the client
-     *    i.e client.get(apiUrl, params, handler);
-     *    i.e client.post(apiUrl, params, handler);
-     */
+			if (maxId != 0) {
+				params.put("max_id", String.valueOf(maxId));
+			}
+
+			if (screenname != null) {
+				params.put("screen_name", screenname);
+			}
+			client.get(apiUrl, params, handler);
+		}
+	}
+	/*
+	 * 1. Define the endpoint URL with getApiUrl and pass a relative path to the
+	 * endpoint i.e getApiUrl("statuses/home_timeline.json"); 2. Define the
+	 * parameters to pass to the request (query or body) i.e RequestParams
+	 * params = new RequestParams("foo", "bar"); 3. Define the request method
+	 * and make a call to the client i.e client.get(apiUrl, params, handler);
+	 * i.e client.post(apiUrl, params, handler);
+	 */
 }
